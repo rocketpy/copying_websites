fb0a892eb62dc3650e9ed609a8a816f356b2f2f4
rocketpy <dimitr.ushakov@gmail.com>
2022-06-07 23:28:43 +0300

    Update tutorial_pywebcopy.py

--- tutorial_pywebcopy.py
+++ tutorial_pywebcopy.py
@@ -1,1 +1,25 @@
+# PyWebCopy is a free tool for copying full or partial websites locally onto your hard-disk for offline viewing.
 
+# https://github.com/rajatomar788/pywebcopy/
+
+# Installation
+pip install pywebcopy
+
+import pywebcopy
+
+# pywebcopy.__version___
+
+
+# To save any single page
+from pywebcopy import save_webpage
+
+save_webpage(
+      url="https://httpbin.org/",
+      project_folder="E://savedpages//",
+      project_name="my_site",
+      bypass_robots=True,
+      debug=True,
+      open_in_browser=True,
+      delay=None,
+      threaded=False,
+)
