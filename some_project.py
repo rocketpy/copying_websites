4f17ccb0e7d5fe7772dcd5b159272c094168b3ea
rocketpy <dimitr.ushakov@gmail.com>
2022-06-13 23:34:55 +0300

    Update some_project.py

--- some_project.py
+++ some_project.py
@@ -1,1 +1,20 @@
+from pywebcopy import save_webpage, save_website
+import validators
 
+
+def warning(text):
+    print("\033[1m\033[31m{}\033[0m".format(text))
+
+def webpage(url, folder, name):
+    save_webpage(
+      url=url,
+		  project_folder=folder,
+		  project_name=name,
+		  bypass_robots=True,
+		  debug=True,
+		  open_in_browser=True,
+		  delay=None,
+		  threaded=False,
+    )
+		
+  
