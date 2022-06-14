c4319d46542935a78e639041a8d30b4ea76ad5e9
rocketpy <dimitr.ushakov@gmail.com>
2022-06-14 23:40:41 +0300

    Update some_project.py

--- some_project.py
+++ some_project.py
@@ -1,3 +1,5 @@
+# Origin taked here: https://habr.com/ru/post/669766/ - Download site
+
 from pywebcopy import save_webpage, save_website
 import validators
 
@@ -6,15 +8,31 @@ def warning(text):
     print("\033[1m\033[31m{}\033[0m".format(text))
 
 def webpage(url, folder, name):
-    save_webpage(
-      url=url,
-		  project_folder=folder,
-		  project_name=name,
-		  bypass_robots=True,
-		  debug=True,
-		  open_in_browser=True,
-		  delay=None,
-		  threaded=False,
+    save_webpage(url=url,
+		 project_folder=folder,
+		 project_name=name,
+		 bypass_robots=True,
+		 debug=True,
+		 open_in_browser=True,
+		 delay=None,
+		 threaded=False,
     )
-		
+
+def website(url, folder, name):
+	save_website(
+		url=url,
+		project_folder=folder,
+		project_name=name,
+		bypass_robots=True,
+		debug=True,
+		open_in_browser=True,
+		delay=None,
+		threaded=False,
+	)
+
+print("""Choose a number:
+1 - Save page
+2 - Save page""")
+
+b=False	
   
